9c5fee8f89b13da45b7e16454bab23686589129b
Charly <charly5bollinger@gmail.com>
2021-01-11 22:12:55 +0100

    Initial commit: empty fabric mod

---
+++ src/main/java/com/github/charlyb01/music_control/MusicControl.java
@@ -0,0 +1,10 @@
+package com.github.charlyb01.music_control;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MusicControl implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/github/charlyb01/music_control/client/MusicControlClient.java
@@ -0,0 +1,13 @@
+package com.github.charlyb01.music_control.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class MusicControlClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
