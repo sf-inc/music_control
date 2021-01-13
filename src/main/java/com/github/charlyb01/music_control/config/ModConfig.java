bf042968926712c9d0c578aeeaaa79068e517567
Charly <charly5bollinger@gmail.com>
2021-01-13 23:54:06 +0100

    Change the frequency of the music playing
    Set the timer between two music playing

--- src/main/java/com/github/charlyb01/music_control/config/ModConfig.java
+++ src/main/java/com/github/charlyb01/music_control/config/ModConfig.java
@@ -1,11 +1,16 @@
 package com.github.charlyb01.music_control.config;
 
+import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
 import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
 import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry;
 
 @Config(name = "music_control")
 public class ModConfig implements ConfigData {
-    @ConfigEntry.BoundedDiscrete(min = 1, max = 100)
-    public int test = 10;
+    @ConfigEntry.BoundedDiscrete(min = 0, max = 300)
+    public int timer = 15;
+
+    public static ModConfig get() {
+        return AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+    }
 }
