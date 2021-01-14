e4ef8cb8967baf61b40c8c7bdfa4db9ccc11a9c5
Charly <charly5bollinger@gmail.com>
2021-01-15 00:17:38 +0100

    Added change category feature

--- src/main/java/com/github/charlyb01/music_control/mixin/MusicTrackerMixin.java
+++ src/main/java/com/github/charlyb01/music_control/mixin/MusicTrackerMixin.java
@@ -1,9 +1,14 @@
 package com.github.charlyb01.music_control.mixin;
 
+import com.github.charlyb01.music_control.categories.MusicCategories;
 import com.github.charlyb01.music_control.client.MusicControlClient;
+import com.github.charlyb01.music_control.config.ModConfig;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.sound.MusicTracker;
 import net.minecraft.client.sound.SoundInstance;
+import net.minecraft.sound.MusicSound;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,11 +16,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Random;
+
 @Mixin(MusicTracker.class)
-public class MusicTrackerMixin {
+public abstract class MusicTrackerMixin {
     @Shadow @Final private MinecraftClient client;
+    @Shadow @Final private Random random;
     @Shadow private int timeUntilNextSong;
     @Shadow private SoundInstance current;
+
+    @Shadow public abstract void play(MusicSound type);
 
     @Inject(at = @At("HEAD"), method = "tick")
     private void tryToSkip(CallbackInfo ci) {
@@ -23,6 +33,18 @@ public class MusicTrackerMixin {
             MusicControlClient.skip = false;
             this.client.getSoundManager().stop(this.current);
             this.timeUntilNextSong = 0;
+        }
+        if (MusicControlClient.category) {
+            MusicControlClient.category = false;
+            MusicCategories.changeCategory(this.random);
+
+            Identifier identifier = MusicCategories.chooseIdentifier(this.random);
+            MusicSound musicSound = new MusicSound(Registry.SOUND_EVENT.get(identifier),
+                    ModConfig.get().timer, ModConfig.get().timer, true);
+
+            this.client.getSoundManager().stop(this.current);
+            this.timeUntilNextSong = 0;
+            play(musicSound);
         }
     }
 }
