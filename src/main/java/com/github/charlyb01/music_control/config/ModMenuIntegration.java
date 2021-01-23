356fa493725b7fcabe66a94ce97c77c4a99a34a1
Charly <charly5bollinger@gmail.com>
2021-01-23 22:38:59 +0100

    Update modmenu api use
    Was creating weird bug when launching the game that can crash

--- src/main/java/com/github/charlyb01/music_control/config/ModMenuIntegration.java
+++ src/main/java/com/github/charlyb01/music_control/config/ModMenuIntegration.java
@@ -1,18 +1,16 @@
 package com.github.charlyb01.music_control.config;
 
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.gui.screen.Screen;
-
-import java.util.function.Function;
 
 @Environment(EnvType.CLIENT)
 public class ModMenuIntegration implements ModMenuApi {
 
     @Override
-    public Function<Screen, ? extends Screen> getConfigScreenFactory() {
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return screen -> AutoConfig.getConfigScreen(ModConfig.class, screen).get();
     }
 }
