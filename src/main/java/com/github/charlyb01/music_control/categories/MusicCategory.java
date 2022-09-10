970a72812bb33a77f539a937f65c7411d2db033d
charlyb01 <charly5bollinger@gmail.com>
2022-09-10 21:25:39 +0200

    Remove old unused code

--- src/main/java/com/github/charlyb01/music_control/categories/MusicCategory.java
+++ src/main/java/com/github/charlyb01/music_control/categories/MusicCategory.java
@@ -5,27 +5,4 @@ public enum MusicCategory {
     DISC,
     CUSTOM,
     ALL
-
-    /*private final HashSet<Identifier> musics = new HashSet<>();
-
-    public Identifier get(final int i) {
-        return (Identifier) this.musics.toArray()[i];
-    }
-
-    public Identifier get(final Random random) {
-        Identifier identifier;
-        int i, size = MusicControlClient.currentCategory.musics.size();
-
-        while (MusicCategories.PLAYED_MUSICS.size() >= Math.min(ModConfig.get().musicQueue, size))
-            MusicCategories.PLAYED_MUSICS.poll();
-
-        do {
-            i = random.nextInt(size);
-            identifier = this.get(i);
-        } while (MusicCategories.PLAYED_MUSICS.contains(identifier) && size > MusicCategories.PLAYED_MUSICS.size());
-
-        MusicCategories.PLAYED_MUSICS.add(identifier);
-
-        return identifier;
-    }*/
 }
